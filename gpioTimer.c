fb6b3ec0661f2648e202b8f6555e8fe42fbbadca
glorieuxf <glorieuxf@connectcom.lu>
2016-12-02 15:54:55 +0000

    Fix gpioTimer first draw

--- gpioTimer.c
+++ gpioTimer.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <chrono>
+#include <thread>
 #include <wiringPi.h>
 
 #define Period 60000
-#define Peak 200
+#define Peak 100
 #define CTS_PIN 22
+void sleep(int period)
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(period));
+}
 
 int main(int argc, char *argv[])
 {
@@ -12,6 +18,7 @@ int main(int argc, char *argv[])
 	pinMode(CTS_PIN, OUTPUT);
 	while(1)
 	{
+printf("Tick\n");
 		digitalWrite(CTS_PIN,1);
 		sleep(Peak);
 		digitalWrite(CTS_PIN,0);

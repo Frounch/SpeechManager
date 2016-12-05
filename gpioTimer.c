b761af1919e4201fc55c9d8536b2e110b568be3c
glorieuxf <glorieuxf@connectcom.lu>
2016-12-05 08:59:51 +0000

    improve gpio timer code

--- gpioTimer.c
+++ gpioTimer.c
@@ -4,9 +4,11 @@
 #include <thread>
 #include <wiringPi.h>
 
-#define Period 60000
-#define Peak 100
-#define CTS_PIN 22
+#define Period 15000
+#define Peak     500
+#define CTS_PIN 7
+
+int count = 1000;
 void sleep(int period)
 {
 	std::this_thread::sleep_for(std::chrono::milliseconds(period));
@@ -14,14 +16,22 @@ void sleep(int period)
 
 int main(int argc, char *argv[])
 {
+	if(argc > 1)
+ 	{
+		count = atoi(argv[1]);
+	}
 	wiringPiSetup();
 	pinMode(CTS_PIN, OUTPUT);
-	while(1)
+
+	printf("Generating %d pulses every ",count);
+	printf("%d sec\n", Period / 1000);
+	while(count > 0)
 	{
-printf("Tick\n");
+		printf("Tick #%d\n",count);
 		digitalWrite(CTS_PIN,1);
 		sleep(Peak);
 		digitalWrite(CTS_PIN,0);
+		count--;
 		sleep(Period-Peak);
 	}
 }

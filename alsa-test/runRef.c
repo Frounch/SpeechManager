fe219a34ed7cbc6e5041e74b58c4fdce9245ac03
glorieuxf <glorieuxf@connectcom.lu>
2016-12-13 10:09:23 +0100

    add alsa-test

---
+++ alsa-test/runRef.c
@@ -0,0 +1,8 @@
+int main(int argc, char* argv[])
+{
+	while(1)
+	{
+		system("./playback_test 8000 1 5 < ref.wav");
+	}
+	
+}
---
+++ alsa-test/runTest.c
@@ -0,0 +1,8 @@
+int main(int argc, char* argv[])
+{
+	while(1)
+	{
+		system("./playback_test 44000 2 5 < test.wav");
+	}
+	
+}

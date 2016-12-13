d0836331f608a624e22f313b8c0876846f9f47e0
glorieuxf <glorieuxf@connectcom.lu>
2016-12-13 12:20:30 +0100

    commit original file

--- alsa-test/playback_test.c
+++ alsa-test/playback_test.c
@@ -1,237 +1,151 @@
+/*
+* Simple sound playback using ALSA API and libasound.
+*
+* Compile:
+* $ cc -o play sound_playback.c -lasound
+* 
+* Usage:
+* $ ./play <sample_rate> <channels> <seconds> < <file>
+* 
+* Examples:
+* $ ./play 44100 2 5 < /dev/urandom
+* $ ./play 22050 1 8 < /path/to/file.wav
+*
+* Copyright (C) 2009 Alessandro Ghedini <alessandro@ghedini.me>
+* --------------------------------------------------------------
+* "THE BEER-WARE LICENSE" (Revision 42):
+* Alessandro Ghedini wrote this file. As long as you retain this
+* notice you can do whatever you want with this stuff. If we
+* meet some day, and you think this stuff is worth it, you can
+* buy me a beer in return.
+* --------------------------------------------------------------
+*/
+
 #include <alsa/asoundlib.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "wav.h"
+#include <fcntl.h>       /* File Control Definitions           */
+#include <termios.h>     /* POSIX Terminal Control Definitions */
+#include <unistd.h>      /* UNIX Standard Definitions          */
+#include <errno.h>       /* ERROR Number Definitions           */
+#include <sys/ioctl.h>   /* ioctl() */
 
-typedef struct
-{
-    char RIFF_marker[4];
-    unsigned int file_size;
-    char filetype_header[4];
-    char format_marker[4];
-    unsigned int data_header_length;
-    unsigned short format_type;
-    unsigned short number_of_channels;
-    unsigned int sample_rate;
-    unsigned int bytes_per_second;
-    unsigned short bytes_per_frame;
-    unsigned short bits_per_sample;
-} WaveHeader;
+#define PCM_DEVICE "default"
 
-WaveHeader *genericWAVHeader(uint32_t sample_rate, uint16_t bit_depth, uint16_t channels)
-{
-	WaveHeader *hdr;
-	hdr = malloc(sizeof(*hdr));
-	if (!hdr) return NULL;
-
-	memcpy(&hdr->RIFF_marker, "RIFF", 4);
-	memcpy(&hdr->filetype_header, "WAVE", 4);
-	memcpy(&hdr->format_marker, "fmt ", 4);
-	hdr->data_header_length = 16;
-	hdr->format_type = 1;
-	hdr->number_of_channels = channels;
-	hdr->sample_rate = sample_rate;
-	hdr->bytes_per_second = sample_rate * channels * bit_depth / 8;
-	hdr->bytes_per_frame = channels * bit_depth / 8;
-	hdr->bits_per_sample = bit_depth;
-
-	return hdr;
-}
-
-int writeWAVHeader(int fd, WaveHeader *hdr)
-{
-	if (!hdr)
-	return -1;
-
-	write(fd, &hdr->RIFF_marker, 4);
-	write(fd, &hdr->file_size, 4);
-	write(fd, &hdr->filetype_header, 4);
-	write(fd, &hdr->format_marker, 4);
-	write(fd, &hdr->data_header_length, 4);
-	write(fd, &hdr->format_type, 2);
-	write(fd, &hdr->number_of_channels, 2);
-	write(fd, &hdr->sample_rate, 4);
-	write(fd, &hdr->bytes_per_second, 4);
-	write(fd, &hdr->bytes_per_frame, 2);
-	write(fd, &hdr->bits_per_sample, 2);
-	write(fd, "data", 4);
-
-	uint32_t data_size = hdr->file_size - 36;
-	write(fd, &data_size, 4);
-
-	return 0;
-}
-
-int recordWAV(const char *fileName, WaveHeader *hdr, uint32_t duration)
-{
-	int err;
-	int size;
-	snd_pcm_t *handle;
+int main(int argc, char **argv) {
+	unsigned int pcm, tmp, dir;
+	int rate, channels, seconds;
+	snd_pcm_t *pcm_handle;
 	snd_pcm_hw_params_t *params;
-	unsigned int sampleRate = hdr->sample_rate;
-	int dir;
-	snd_pcm_uframes_t frames = 32;
-	const char *device = "plughw:1,0"; // USB microphone
-	// const char *device = "default"; // Integrated system microphone
-	char *buffer;
-	int filedesc;
+	snd_pcm_uframes_t frames;
+	char *buff;
+	int buff_size, loops;
 
-	/* Open PCM device for recording (capture). */
-	err = snd_pcm_open(&handle, device, SND_PCM_STREAM_CAPTURE, 0);
-	if (err)
-	{
-		fprintf(stderr, "Unable to open PCM device: %s\n", snd_strerror(err));
-		return err;
-	}
-
-	/* Allocate a hardware parameters object. */
-	snd_pcm_hw_params_alloca(&params);
-
-	/* Fill it in with default values. */
-	snd_pcm_hw_params_any(handle, params);
-
-	/* ### Set the desired hardware parameters. ### */
-
-	/* Interleaved mode */
-	err = snd_pcm_hw_params_set_access(handle, params, SND_PCM_ACCESS_RW_INTERLEAVED);
-	if (err)
-	{
-		fprintf(stderr, "Error setting interleaved mode: %s\n", snd_strerror(err));
-		snd_pcm_close(handle);
-		return err;
-	}
-	/* Signed 16-bit little-endian format */
-	if (hdr->bits_per_sample == 16) err = snd_pcm_hw_params_set_format(handle, params, SND_PCM_FORMAT_S16_LE);
-	else err = snd_pcm_hw_params_set_format(handle, params, SND_PCM_FORMAT_U8);
-	if (err)
-	{
-		fprintf(stderr, "Error setting format: %s\n", snd_strerror(err));
-		snd_pcm_close(handle);
-		return err;
-	}
-	/* Two channels (stereo) */
-	err = snd_pcm_hw_params_set_channels(handle, params, hdr->number_of_channels);
-	if (err)
-	{
-		fprintf(stderr, "Error setting channels: %s\n", snd_strerror(err));
-		snd_pcm_close(handle);
-		return err;
-	}
-	/* 44100 bits/second sampling rate (CD quality) */
-	sampleRate = hdr->sample_rate;
-	err = snd_pcm_hw_params_set_rate_near(handle, params, &sampleRate, &dir);
-	if (err)
-	{
-		fprintf(stderr, "Error setting sampling rate (%d): %s\n", sampleRate, snd_strerror(err));
-		snd_pcm_close(handle);
-		return err;
-	}
-	hdr->sample_rate = sampleRate;
-	/* Set period size*/
-	err = snd_pcm_hw_params_set_period_size_near(handle, params, &frames, &dir);
-	if (err)
-	{
-		fprintf(stderr, "Error setting period size: %s\n", snd_strerror(err));
-		snd_pcm_close(handle);
-		return err;
-	}
-	/* Write the parameters to the driver */
-	err = snd_pcm_hw_params(handle, params);
-	if (err < 0)
-	{
-		fprintf(stderr, "Unable to set HW parameters: %s\n", snd_strerror(err));
-		snd_pcm_close(handle);
-		return err;
-	}
-
-	/* Use a buffer large enough to hold one period */
-	err = snd_pcm_hw_params_get_period_size(params, &frames, &dir);
-	if (err)
-	{
-		fprintf(stderr, "Error retrieving period size: %s\n", snd_strerror(err));
-		snd_pcm_close(handle);
-		return err;
-	}
-
-	size = frames * hdr->bits_per_sample / 8 * hdr->number_of_channels; /* 2 bytes/sample, 2 channels */
-	buffer = (char *) malloc(size);
-	if (!buffer)
-	{
-		fprintf(stdout, "Buffer error.\n");
-		snd_pcm_close(handle);
+	if (argc < 4) {
+		printf("Usage: %s <sample_rate> <channels> <seconds>\n",
+		argv[0]);
 		return -1;
 	}
 
-	err = snd_pcm_hw_params_get_period_time(params, &sampleRate, &dir);
-	if (err)
-	{
-		fprintf(stderr, "Error retrieving period time: %s\n", snd_strerror(err));
-		snd_pcm_close(handle);
-		free(buffer);
-		return err;
-	}
-
-	uint32_t pcm_data_size = hdr->sample_rate * hdr->bytes_per_frame * (duration / 1000);
-	printf("data_size : %d", pcm_data_size);
-	hdr->file_size = pcm_data_size + 36;
-
-	filedesc = open(fileName, O_WRONLY | O_CREAT, 0644);
-	err = writeWAVHeader(filedesc, hdr);
-	if (err)
-	{
-		fprintf(stderr, "Error writing .wav header.");
-		snd_pcm_close(handle);
-		free(buffer);
-		close(filedesc);
-		return err;
-	}
-	int totalFrames = 0;
-	int length = ((duration * 1000) / (hdr->sample_rate / frames));
-	printf("for length %d", length);
-	for(int i = ((duration * 1000) / (hdr->sample_rate / frames)); i > 0; i--)
-	{
-		err = snd_pcm_readi(handle, buffer, frames);
-		totalFrames += err;
-		if (err == -EPIPE) fprintf(stderr, "Overrun occurred: %d\n", err);
-		if (err < 0) err = snd_pcm_recover(handle, err, 0);
-		// Still an error, need to exit.
-		if (err < 0)
-		{
-			fprintf(stderr, "Error occured while recording: %s\n", snd_strerror(err));
-			snd_pcm_close(handle);
-			free(buffer);
-			close(filedesc);
-			return err;
-		}
-		write(filedesc, buffer, size);
-	}
-
-	close(filedesc);
-	snd_pcm_drain(handle);
-	snd_pcm_close(handle);
-	free(buffer);
-	return 0;
-}
-
-int main(int argc, char* argv[])
-{
 	// Init serial
 	int serial = open("/dev/ttyS1", O_RDWR | O_NOCTTY);
 	int status;
 	unsigned int mask = TIOCM_CTS;
 	
-	do
-	{
-		// Wait for CTS change
-		ioctl(serial, TIOCMIWAIT, mask);
-		
-		// Read CTS state
-		ioctl(serial, TIOCMGET, &status);
-	}
-	while(!(status & TIOCM_CTS));
+	rate 	 = atoi(argv[1]);
+	channels = atoi(argv[2]);
+	seconds  = atoi(argv[3]);
 
-	WaveHeader * hdr = genericWAVHeader(44000, 16, 2);
-	char fileName[] = "rec.wav";
-	recordWAV(fileName, hdr, 5000);	
-} 
+	/* Open the PCM device in playback mode */
+	if (pcm = snd_pcm_open(&pcm_handle, PCM_DEVICE,
+				SND_PCM_STREAM_PLAYBACK, 0) < 0) 
+	printf("ERROR: Can't open \"%s\" PCM device. %s\n",
+	PCM_DEVICE, snd_strerror(pcm));
+
+	/* Allocate parameters object and fill it with default values*/
+	snd_pcm_hw_params_alloca(&params);
+
+	snd_pcm_hw_params_any(pcm_handle, params);
+
+	/* Set parameters */
+	if (pcm = snd_pcm_hw_params_set_access(pcm_handle, params,
+				SND_PCM_ACCESS_RW_INTERLEAVED) < 0) 
+	printf("ERROR: Can't set interleaved mode. %s\n", snd_strerror(pcm));
+
+	if (pcm = snd_pcm_hw_params_set_format(pcm_handle, params,
+				SND_PCM_FORMAT_S16_LE) < 0) 
+	printf("ERROR: Can't set format. %s\n", snd_strerror(pcm));
+
+	if (pcm = snd_pcm_hw_params_set_channels(pcm_handle, params, channels) < 0) 
+	printf("ERROR: Can't set channels number. %s\n", snd_strerror(pcm));
+
+	if (pcm = snd_pcm_hw_params_set_rate_near(pcm_handle, params, &rate, 0) < 0) 
+	printf("ERROR: Can't set rate. %s\n", snd_strerror(pcm));
+
+	/* Write parameters */
+	if (pcm = snd_pcm_hw_params(pcm_handle, params) < 0)
+	printf("ERROR: Can't set harware parameters. %s\n", snd_strerror(pcm));
+
+	/* Resume information */
+	printf("PCM name: '%s'\n", snd_pcm_name(pcm_handle));
+
+	printf("PCM state: %s\n", snd_pcm_state_name(snd_pcm_state(pcm_handle)));
+
+	snd_pcm_hw_params_get_channels(params, &tmp);
+	printf("channels: %i ", tmp);
+
+	if (tmp == 1)
+	printf("(mono)\n");
+	else if (tmp == 2)
+	printf("(stereo)\n");
+
+	snd_pcm_hw_params_get_rate(params, &tmp, 0);
+	printf("rate: %d bps\n", tmp);
+
+	printf("seconds: %d\n", seconds);	
+
+	while(1)
+	{
+		/* Allocate buffer to hold single period */
+		snd_pcm_hw_params_get_period_size(params, &frames, 0);
+
+		buff_size = frames * channels * 2 /* 2 -> sample size */;
+		buff = (char *) malloc(buff_size);
+
+		snd_pcm_hw_params_get_period_time(params, &tmp, NULL);
+		
+		do
+		{
+			// Wait for CTS change
+			ioctl(serial, TIOCMIWAIT, mask);
+			
+			// Read CTS state
+			ioctl(serial, TIOCMGET, &status);
+		}
+		while(!(status & TIOCM_CTS));
+		
+		for (loops = (seconds * 1000000) / tmp; loops > 0; loops--) 
+		{
+			if (pcm = read(0, buff, buff_size) == 0) {
+				printf("Early end of file.\n");
+				return 0;
+			}
+
+			if (pcm = snd_pcm_writei(pcm_handle, buff, frames) == -EPIPE) 
+			{
+				printf("XRUN.\n");
+				snd_pcm_prepare(pcm_handle);
+			} else if (pcm < 0) 
+			{
+				printf("ERROR. Can't write to PCM device. %s\n", snd_strerror(pcm));
+			}
+		}
+		
+		
+		snd_pcm_drain(pcm_handle);
+		snd_pcm_rewind(pcm_handle, frames);
+		snd_pcm_close(pcm_handle);
+	}
+	free(buff);
+
+	return 0;
+}

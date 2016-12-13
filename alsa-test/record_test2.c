ef003b984f8a932c312dd01f3cb581e88a3c4d39
glorieuxf <glorieuxf@connectcom.lu>
2016-12-13 11:29:34 +0100

    record test working

---
+++ alsa-test/record_test2.c
@@ -0,0 +1,361 @@
+#include <alsa/asoundlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <fcntl.h>       /* File Control Definitions           */
+#include <termios.h>     /* POSIX Terminal Control Definitions */
+#include <unistd.h>      /* UNIX Standard Definitions          */
+#include <errno.h>       /* ERROR Number Definitions           */
+#include <sys/ioctl.h>   /* ioctl() */
+
+typedef struct
+{
+    char RIFF_marker[4];
+    unsigned int file_size;
+    char filetype_header[4];
+    char format_marker[4];
+    unsigned int data_header_length;
+    unsigned short format_type;
+    unsigned short number_of_channels;
+    unsigned int sample_rate;
+    unsigned int bytes_per_second;
+    unsigned short bytes_per_frame;
+    unsigned short bits_per_sample;
+} WaveHeader;
+
+WaveHeader *genericWAVHeader(unsigned int sample_rate, unsigned short bit_depth, unsigned short channels)
+{
+    WaveHeader *hdr;
+    hdr = malloc(sizeof(*hdr));
+    if (!hdr) return NULL;
+
+    memcpy(&hdr->RIFF_marker, "RIFF", 4);
+    memcpy(&hdr->filetype_header, "WAVE", 4);
+    memcpy(&hdr->format_marker, "fmt ", 4);
+    hdr->data_header_length = 16;
+    hdr->format_type = 1;
+    hdr->number_of_channels = channels;
+    hdr->sample_rate = sample_rate;
+    hdr->bytes_per_second = sample_rate * channels * bit_depth / 8;
+    hdr->bytes_per_frame = channels * bit_depth / 8;
+    hdr->bits_per_sample = bit_depth;
+
+    return hdr;
+}
+
+int writeWAVHeader(int fd, WaveHeader *hdr)
+{
+    if (!hdr)
+        return -1;
+
+    write(fd, &hdr->RIFF_marker, 4);
+    write(fd, &hdr->file_size, 4);
+    write(fd, &hdr->filetype_header, 4);
+    write(fd, &hdr->format_marker, 4);
+    write(fd, &hdr->data_header_length, 4);
+    write(fd, &hdr->format_type, 2);
+    write(fd, &hdr->number_of_channels, 2);
+    write(fd, &hdr->sample_rate, 4);
+    write(fd, &hdr->bytes_per_second, 4);
+    write(fd, &hdr->bytes_per_frame, 2);
+    write(fd, &hdr->bits_per_sample, 2);
+    write(fd, "data", 4);
+
+    unsigned int data_size = hdr->file_size - 36;
+    write(fd, &data_size, 4);
+
+    return 0;
+}
+
+int recordWAV(const char *fileName, WaveHeader *hdr, unsigned int duration)
+{
+    int err;
+    int size;
+    snd_pcm_t *handle;
+    snd_pcm_hw_params_t *params;
+    unsigned int sampleRate = hdr->sample_rate;
+    int dir;
+    snd_pcm_uframes_t frames = 32;
+    // const char *device = "hw"; // USB microphone
+    const char *device = "default"; // Integrated system microphone
+    char *buffer;
+    int filedesc;
+
+    /* Open PCM device for recording (capture). */
+    err = snd_pcm_open(&handle, device, SND_PCM_STREAM_CAPTURE, 0);
+    if (err)
+    {
+        fprintf(stderr, "Unable to open PCM device: %s\n", snd_strerror(err));
+        return err;
+    }
+
+    /* Allocate a hardware parameters object. */
+    snd_pcm_hw_params_alloca(&params);
+
+    /* Fill it in with default values. */
+    snd_pcm_hw_params_any(handle, params);
+
+    /* ### Set the desired hardware parameters. ### */
+
+    /* Interleaved mode */
+    err = snd_pcm_hw_params_set_access(handle, params, SND_PCM_ACCESS_RW_INTERLEAVED);
+    if (err)
+    {
+        fprintf(stderr, "Error setting interleaved mode: %s\n", snd_strerror(err));
+        snd_pcm_close(handle);
+        return err;
+    }
+    /* Signed 16-bit little-endian format */
+    if (hdr->bits_per_sample == 16) err = snd_pcm_hw_params_set_format(handle, params, SND_PCM_FORMAT_S16_LE);
+    else err = snd_pcm_hw_params_set_format(handle, params, SND_PCM_FORMAT_U8);
+    if (err)
+    {
+        fprintf(stderr, "Error setting format: %s\n", snd_strerror(err));
+        snd_pcm_close(handle);
+        return err;
+    }
+    /* Two channels (stereo) */
+    err = snd_pcm_hw_params_set_channels(handle, params, hdr->number_of_channels);
+    if (err)
+    {
+        fprintf(stderr, "Error setting channels: %s\n", snd_strerror(err));
+        snd_pcm_close(handle);
+        return err;
+    }
+    /* 44100 bits/second sampling rate (CD quality) */
+    sampleRate = hdr->sample_rate;
+    err = snd_pcm_hw_params_set_rate_near(handle, params, &sampleRate, &dir);
+    if (err)
+    {
+        fprintf(stderr, "Error setting sampling rate (%d): %s\n", sampleRate, snd_strerror(err));
+        snd_pcm_close(handle);
+        return err;
+    }
+    hdr->sample_rate = sampleRate;
+    /* Set period size*/
+    err = snd_pcm_hw_params_set_period_size_near(handle, params, &frames, &dir);
+    if (err)
+    {
+        fprintf(stderr, "Error setting period size: %s\n", snd_strerror(err));
+        snd_pcm_close(handle);
+        return err;
+    }
+    /* Write the parameters to the driver */
+    err = snd_pcm_hw_params(handle, params);
+    if (err < 0)
+    {
+        fprintf(stderr, "Unable to set HW parameters: %s\n", snd_strerror(err));
+        snd_pcm_close(handle);
+        return err;
+    }
+
+    /* Use a buffer large enough to hold one period */
+    err = snd_pcm_hw_params_get_period_size(params, &frames, &dir);
+    if (err)
+    {
+        fprintf(stderr, "Error retrieving period size: %s\n", snd_strerror(err));
+        snd_pcm_close(handle);
+        return err;
+    }
+
+    size = frames * hdr->bits_per_sample / 8 * hdr->number_of_channels; /* 2 bytes/sample, 2 channels */
+    buffer = (char *) malloc(size);
+    if (!buffer)
+    {
+        fprintf(stdout, "Buffer error.\n");
+        snd_pcm_close(handle);
+        return -1;
+    }
+
+    err = snd_pcm_hw_params_get_period_time(params, &sampleRate, &dir);
+    if (err)
+    {
+        fprintf(stderr, "Error retrieving period time: %s\n", snd_strerror(err));
+        snd_pcm_close(handle);
+        free(buffer);
+        return err;
+    }
+
+    unsigned int pcm_data_size = hdr->sample_rate * hdr->bytes_per_frame * (duration / 1000);
+    hdr->file_size = pcm_data_size + 36;
+
+    filedesc = open(fileName, O_WRONLY | O_CREAT, 0644);
+    err = writeWAVHeader(filedesc, hdr);
+    if (err)
+    {
+        fprintf(stderr, "Error writing .wav header.");
+        snd_pcm_close(handle);
+        free(buffer);
+        close(filedesc);
+        return err;
+    }
+    int totalFrames = 0;
+	int i;
+    for(i = ((duration * 1000) / (hdr->sample_rate / frames)); i > 0; i--)
+    {
+        err = snd_pcm_readi(handle, buffer, frames);
+        totalFrames += err;
+        if (err == -EPIPE) fprintf(stderr, "Overrun occurred: %d\n", err);
+        if (err < 0) err = snd_pcm_recover(handle, err, 0);
+        // Still an error, need to exit.
+        if (err < 0)
+        {
+            fprintf(stderr, "Error occured while recording: %s\n", snd_strerror(err));
+            snd_pcm_close(handle);
+            free(buffer);
+            close(filedesc);
+            return err;
+        }
+        write(filedesc, buffer, size);
+    }
+	printf("frames : %d\n",totalFrames);
+    close(filedesc);
+    snd_pcm_drain(handle);
+    snd_pcm_close(handle);
+    free(buffer);
+    return 0;
+}
+
+/* int main(int argc, char* argv[])
+{
+	// Init serial
+	int serial = open("/dev/ttyS1", O_RDWR | O_NOCTTY);
+	int status;
+	unsigned int mask = TIOCM_CTS;
+	
+	do
+	{
+		// Wait for CTS change
+		ioctl(serial, TIOCMIWAIT, mask);
+		
+		// Read CTS state
+		ioctl(serial, TIOCMGET, &status);
+	}
+	while(!(status & TIOCM_CTS));
+
+	WaveHeader * hdr = genericWAVHeader(44000, 16, 2);
+	char fileName[] = "rec.wav";
+	recordWAV(fileName, hdr, 5);	
+} */
+
+main (int argc, char *argv[])
+{
+	int i;
+	int err;
+	char *buffer;
+	int buff_size, buffer_frames = 128;
+	unsigned int rate = 44100;
+	snd_pcm_t *capture_handle;
+	snd_pcm_hw_params_t *hw_params;
+	snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE;
+
+	char fileName[] = "rec.wav";
+	WaveHeader * hdr = genericWAVHeader(44100, 16, 2);
+	int filedesc;
+
+	if ((err = snd_pcm_open (&capture_handle, argv[1], SND_PCM_STREAM_CAPTURE, 0)) < 0) {
+		fprintf (stderr, "cannot open audio device %s (%s)\n", 
+		argv[1],
+		snd_strerror (err));
+		exit (1);
+	}
+
+	fprintf(stdout, "audio interface opened\n");
+
+	if ((err = snd_pcm_hw_params_malloc (&hw_params)) < 0) {
+		fprintf (stderr, "cannot allocate hardware parameter structure (%s)\n",
+		snd_strerror (err));
+		exit (1);
+	}
+
+	fprintf(stdout, "hw_params allocated\n");
+
+	if ((err = snd_pcm_hw_params_any (capture_handle, hw_params)) < 0) {
+		fprintf (stderr, "cannot initialize hardware parameter structure (%s)\n",
+		snd_strerror (err));
+		exit (1);
+	}
+
+	fprintf(stdout, "hw_params initialized\n");
+	
+	if ((err = snd_pcm_hw_params_set_access (capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
+		fprintf (stderr, "cannot set access type (%s)\n",
+		snd_strerror (err));
+		exit (1);
+	}
+
+	fprintf(stdout, "hw_params access setted\n");
+	
+	if ((err = snd_pcm_hw_params_set_format (capture_handle, hw_params, format)) < 0) {
+		fprintf (stderr, "cannot set sample format (%s)\n",
+		snd_strerror (err));
+		exit (1);
+	}
+
+	fprintf(stdout, "hw_params format setted\n");
+	
+	if ((err = snd_pcm_hw_params_set_rate_near (capture_handle, hw_params, &rate, 0)) < 0) {
+		fprintf (stderr, "cannot set sample rate (%s)\n",
+		snd_strerror (err));
+		exit (1);
+	}
+	
+	fprintf(stdout, "hw_params rate setted\n");
+
+	if ((err = snd_pcm_hw_params_set_channels (capture_handle, hw_params, 2)) < 0) {
+		fprintf (stderr, "cannot set channel count (%s)\n",
+		snd_strerror (err));
+		exit (1);
+	}
+
+	fprintf(stdout, "hw_params channels setted\n");
+	
+	if ((err = snd_pcm_hw_params (capture_handle, hw_params)) < 0) {
+		fprintf (stderr, "cannot set parameters (%s)\n",
+		snd_strerror (err));
+		exit (1);
+	}
+
+	fprintf(stdout, "hw_params setted\n");
+	
+	snd_pcm_hw_params_free (hw_params);
+
+	fprintf(stdout, "hw_params freed\n");
+	
+	if ((err = snd_pcm_prepare (capture_handle)) < 0) {
+		fprintf (stderr, "cannot prepare audio interface for use (%s)\n",
+		snd_strerror (err));
+		exit (1);
+	}
+
+	fprintf(stdout, "audio interface prepared\n");
+
+	buff_size = 128 * snd_pcm_format_width(format) / 8 * 2;
+	buffer = malloc(buff_size);
+
+	fprintf(stdout, "buffer allocated\n");
+	
+	filedesc = open(fileName, O_WRONLY | O_CREAT, 0644);
+    writeWAVHeader(filedesc, hdr);
+
+	for (i = 0; i < 1000; ++i) {
+		if ((err = snd_pcm_readi (capture_handle, buffer, buffer_frames)) != buffer_frames) {
+			fprintf (stderr, "read from audio interface failed (%s)\n",
+			err, snd_strerror (err));
+			exit (1);
+		}
+		write(filedesc, buffer, buff_size);
+	}
+	
+	fprintf(stdout, "read done\n");
+	close(filedesc);
+	free(buffer);
+
+	fprintf(stdout, "buffer freed\n");
+	
+	snd_pcm_close (capture_handle);
+	fprintf(stdout, "audio interface closed\n");
+
+	exit (0);
+}
